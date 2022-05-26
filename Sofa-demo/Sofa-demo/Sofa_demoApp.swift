9ea46a4c576b2861248aee7c66477183c75e1f71
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-05-26 12:53:14 +0900

    [FEAT] Sofa-demo Proj

---
+++ Sofa-demo/Sofa-demo/Sofa_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sofa_demoApp.swift
+//  Sofa-demo
+//
+//  Created by 양유진 on 2022/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Sofa_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
