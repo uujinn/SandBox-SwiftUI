cbe5440e949f6277aa4a8cc2ef9b6ca768bd3c68
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-06-23 17:12:06 +0900

    [FEAT] ReduxTest

---
+++ Practice/ReduxTest/ReduxTest/DiceView.swift
@@ -0,0 +1,20 @@
+//
+//  DiceView.swift
+//  ReduxTest
+//
+//  Created by 양유진 on 2022/06/23.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiceView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiceView()
+    }
+}
---
+++ Practice/ReduxTest/ReduxTest/ReduxTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxTestApp.swift
+//  ReduxTest
+//
+//  Created by 양유진 on 2022/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ReduxTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
