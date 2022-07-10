c18360f447d7914bd262b9b5c76ccb70058a704a
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-07-10 16:46:36 +0900

    [FEAT] BulbTest

---
+++ Practice/BulbTest/BulbTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BulbTest
+//
+//  Created by 양유진 on 2022/07/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
