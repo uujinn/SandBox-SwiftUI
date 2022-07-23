b6fcd6a07542cbc9045d1004cc26d9956c085d67
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-07-24 02:36:32 +0900

    [FEAT] StarsCreamTest

---
+++ Sofa/StarsCreamTest/StarsCreamTest/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  StarsCreamTest
+//
+//  Created by 양유진 on 2022/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  
+  let socket = websocket()
+  
+  @State var text = ""
+  @State var receivedText = ""
+  var function: (() -> Void)?
+  
+  var body: some View {
+    VStack{
+      Text("\(receivedText)")
+      
+      TextField("Enter", text: $text)
+        .padding()
+        .background(Color.gray)
+        .opacity(0.4)
+      
+      
+      Button {
+        print("Send")
+        self.socket.send(message: text)
+      } label: {
+        Text("Send")
+          .padding()
+          .background(Color.pink)
+      }
+
+    }
+    .onAppear{
+      self.socket.connect()
+    }
+    
+  }
+
+  func receivedMessage(newText: String){
+    self.receivedText = newText
+  }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
